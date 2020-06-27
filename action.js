24cdf44223fa3fd2810287aeb05d8b6ad3aea4ba
j900213 <j900213@gmail.com>
2020-06-27 23:32:24 +0200

    Fixed sensitivity problem

--- action.js
+++ action.js
@@ -5,20 +5,19 @@ const target = document.querySelectorAll(".magic-box");
 for (let i = 0; i < target.length; i++) {
   // Add bg when hovering
   target[i].addEventListener("mouseenter", function () {
-    target[i].classList.remove("magic-fade");
-    target[i].classList.add("magic-show");
+    target[i].parentNode.classList.add("bgcolor");
     switch (i + 1) {
       case 1:
-        target[i].classList.add("bgcolor-1");
+        target[i].parentNode.classList.add("bgcolor-1");
         break;
       case 2:
-        target[i].classList.add("bgcolor-2");
+        target[i].parentNode.classList.add("bgcolor-2");
         break;
       case 3:
-        target[i].classList.add("bgcolor-3");
+        target[i].parentNode.classList.add("bgcolor-3");
         break;
       case 4:
-        target[i].classList.add("bgcolor-4");
+        target[i].parentNode.classList.add("bgcolor-4");
         break;
       default:
         break;
@@ -27,7 +26,24 @@ for (let i = 0; i < target.length; i++) {
 
   // Add fade out animation when mouse out
   target[i].addEventListener("mouseleave", function () {
-    target[i].classList.add("magic-fade");
-    target[i].classList.remove("magic-show");
+    setTimeout(function () {
+      target[i].parentNode.classList.remove("bgcolor");
+      switch (i + 1) {
+        case 1:
+          target[i].parentNode.classList.remove("bgcolor-1");
+          break;
+        case 2:
+          target[i].parentNode.classList.remove("bgcolor-2");
+          break;
+        case 3:
+          target[i].parentNode.classList.remove("bgcolor-3");
+          break;
+        case 4:
+          target[i].parentNode.classList.remove("bgcolor-4");
+          break;
+        default:
+          break;
+      }
+    }, 2300);
   });
 }
